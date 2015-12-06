161e92986e388a7e00b2b6de03e0a67627d2c769
siukwan <siukwan@localhost.localdomain>
2015-12-06 10:36:48 +0800

    1206

--- 20151204tcpGetTime/main.cpp
+++ 20151204tcpGetTime/main.cpp
@@ -5,16 +5,27 @@
 #define MAXLINE 4096
 using namespace std;
 
+void err_quit(string str)
+{
+cout<<str<<endl;
+//exit(0);
+}
+void err_sys(string str)
+{
+cout<<str<<endl;
+//exit(0);
+}
+
 int main(int argc,char **argv)
 {
     int sockfd,n;
     char recvline[MAXLINE+1];
-    struct sockaddr_in servaddr;//the sockaddr_in defination is in the headfile <netinet/in.h>
+    sockaddr_in servaddr;//the sockaddr_in defination is in the headfile <netinet/in.h>
 
     if(argc !=2)
         err_quit("usage: a.put <IPaddress>");
 
-    if((sockfd=socket(AF_INET,SOCK_STREAM<,0))<0)
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
         err_sys("socket error");
     bzero(&servaddr,sizeof(servaddr));
     cout<<"test"<<endl;
