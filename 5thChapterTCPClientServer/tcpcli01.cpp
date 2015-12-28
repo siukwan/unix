e688af12f471d137708cf682918c3ca00099aad2
siukwan <siukwan@siukwandeAir.lan>
2015-12-28 13:38:06 +0800

    add files

--- 5thChapterTCPClientServer/tcpcli01.cpp
+++ 5thChapterTCPClientServer/tcpcli01.cpp
@@ -113,68 +113,7 @@ static int read_cnt;
 static char *read_ptr;
 static char read_buf[MAXLINE];
 
-static ssize_t my_read(int fd, char *ptr)
-{// 读取输入
-	if(read_cnt <= 0 )
-	{
-again:
-		if( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0 )
-		{
-			if(errno == EINTR)//如果因为信号中断导致读写失败，则重新读取
-				goto again;
-			return (-1);
-		}
-		else if(read_cnt == 0 )
-			return (0);
-		read_ptr = read_buf;
-	}
-	read_cnt--;
-	*ptr = *read_ptr++;
-	return (1);
-}
 
-ssize_t readline( int fd, void *vptr, size_t maxlen)
-{
-	ssize_t n, rc;
-	char    c, *ptr;
-	ptr = (char *) vptr;
-	for( n = 1; n < maxlen; n++ )
-	{
-		if( (rc = my_read(fd, &c)) == 1)
-		{
-			*ptr++ = c;
-			if(c=='\n')
-				break;
-		}
-		else if(rc == 0)
-		{
-			*ptr = 0;
-			return (n-1);
-		}
-		else 
-			return (-1);
-	}
-
-	*ptr = 0;
-	return (n);
-}
-
-ssize_t readlinebuf(void **vptrptr)
-{
-	if(read_cnt)
-		*vptrptr = read_ptr;
-	return (read_cnt);
-}
-
-ssize_t Readline(int fd, void *ptr, size_t maxlen)
-{
-	printf("正在执行Readline函数..\n");
-	ssize_t n;
-	if( (n=readline(fd,ptr,maxlen))<0)
-		err_sys("readline error");
-	printf("Readline函数执行完毕，正在返回%ld\n",n);
-	return (n);
-}
 void Fputs(const char *ptr, FILE *stream)
 {
 	if(fputs(ptr,stream)==EOF)
--- 5thChapterTCPClientServer/tcpserv01-single-thread.cpp
+++ 5thChapterTCPClientServer/tcpserv01-single-thread.cpp
@@ -151,29 +151,29 @@ void str_echo(int sockfd)
 {
 	ssize_t n=0;
 	char    buf[MAXLINE];
-//	printf("准备读区数据...\n");
+	printf("准备读区数据...\n");
 	while(1)
 	{
 		memset(buf,0,MAXLINE);
-//		printf("进入while(1)循环..\n");
+		printf("进入while(1)循环..\n");
 		while( (n=read(sockfd, buf, MAXLINE)) >0 )
 		{
-//			printf("接收到的内容：%s准备进行发送...\n",buf);
+			printf("接收到的内容：%s准备进行发送...\n",buf);
 			Writen(sockfd ,buf, n);
-//			printf("已经发送：%s\n",buf);
+			printf("已经发送：%s\n",buf);
 			memset(buf,0,MAXLINE);
 		}
 
 		if( n<0 && errno == EINTR )
 		{ 
-//			printf("error\n");
+			printf("error\n");
 			continue;
 		}
 		else if(n<0)
 			err_sys("str_echo: read error");
 		else if(n==0)
 		{
-//			printf("n大小为：%ld，循环结束\n\n",n);
+			printf("n大小为：%ld，循环结束\n\n",n);
 			return;	
 		}
 	
@@ -214,19 +214,17 @@ int main(int argc, char **argv)
 		chilen=sizeof(cliaddr);
 		connfd = Accept(listenfd,(SA *)&cliaddr, &chilen);/*read the info in the cliaddr*/
 		cout<<"Forking..."<<endl;
-		if( (childpid = Fork()) == 0 )
-		{/*if pid == 0, it is the child process*/
+		//if( (childpid = Fork()) == 0 )
+		//{/*if pid == 0, it is the child process*/
 			/*close listening socket*/
-			Close(listenfd);
+		//	Close(listenfd);
 
 			/*process the request , return whatever server recieved*/
 			str_echo(connfd);
 
 			/*child process have finished the work ,then exits*/
-		
-			exit(0);
-		}
-		printf("派生子线程%d\n",childpid);
+		//	exit(0);
+		//}
 		cout<<"Forked!"<<endl;
 		Close(connfd);
 	}
