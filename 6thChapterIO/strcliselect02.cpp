bda3e5ac5a29a3ab9768bb1b60eeaf3818a9805f
siukwanCentOS <pypy39353@qq.com>
2016-01-01 22:54:52 +0800

    增加select02

---
+++ 6thChapterIO/strcliselect02.cpp
@@ -0,0 +1,65 @@
+/*
+P137 strcliselect02
+*/
+#include"func.h"
+using namespace std;
+/*
+ * 第五章
+ * 服务器关闭，客户端阻塞于stdin，输入字符串后，进行发送才发现服务器关闭，退出程序
+ * 第六章
+ * 服务器关闭，客户端的select检测到连接断开，直接检测到，然后退出程序
+ * */
+void str_cli_select02(FILE *fp,int sockfd)
+{
+	//stdineof为输入eof标志符
+	bool stdineof=false;
+	int maxfdp1;
+	fd_set rset;
+
+	char buf[MAXLINE];
+	FD_ZERO(&rset);
+	while(1)
+	{
+		//如果标准输入还没读到EOF，继续把fp的描述符设置为监听
+		if(!stdineof)
+			FD_SET(fileno(fp), &rset);
+		FD_SET(sockfd,&rset);
+		maxfdp1 = max(fileno(fp),sockfd) +1;
+		printf("进行select中...\n");
+		Select(maxfdp1,&rset,NULL,NULL,NULL);
+
+		//socket is readable
+		if(FD_ISSET(sockfd,&rset))
+		{
+			if(read(sockfd,buf,MAXLINE)==0)
+			{
+				//如果stdin输入结束，并且sockfd读取也结束，则return
+				if(stdineof)
+					return ;
+				else
+					err_quit("str_cli:server terminated prematurely");
+			}
+			//打印输出
+			printf("接收到的字符串：\n");
+			Write(fileno(stdout),buf,strlen(buf));
+
+		}
+
+		//input is readable
+		if(FD_ISSET(fileno(fp),&rset))
+		{
+			//文件已经读取完毕，关闭socket的发送功能
+			if(read(fileno(fp),buf,MAXLINE) == 0)
+			{
+				stdineof=1;
+				Shutdown(sockfd,SHUT_WR);//send FIN
+				FD_CLR(fileno(fp),&rset);
+				continue;
+			}
+				return ;
+			printf("输入的字符串为：%s，进行发送...\n",buf);
+			Writen(sockfd,buf,strlen(buf));
+		}
+		memset(buf,0,MAXLINE);
+	}
+}
