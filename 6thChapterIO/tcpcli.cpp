654de07246b60ca2f4adce0cd81ca7c07b2fe0a6
siukwanCentOS <pypy39353@qq.com>
2016-01-01 13:09:45 +0800

    优化tcpcli.cpp，当不输入ip地址时，默认为127.0.0.1

--- 6thChapterIO/tcpcli.cpp
+++ 6thChapterIO/tcpcli.cpp
@@ -28,21 +28,37 @@ int main(int argc,char **argv)
 {
 	//开启5个socket
 	int sockfd,n;
+
 	sockaddr_in servaddr;//sockaddr_in in <sys/un.h>
+
+	char ip_default[]="127.0.0.1";
+	char *ip_address;
 	if(argc !=2)
-		err_quit("usage: ./client <IPaddress> For an example: ./client 127.0.0.1");
+	{
+		printf("usage: ./client <IPaddress> For an example: ./client 192.168.1.111\n");
+		printf("Now,the default IP address is 127.0.0.1");
+		ip_address=ip_default;
+	}
+	else
+		ip_address = argv[1];
 	/*socket*/
 	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
 	/*init */
 	bzero(&servaddr,sizeof(servaddr));//对sockaddr结构体进行初始化清零
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(13);//时间服务器的端口
+	servaddr.sin_port = htons(9877);//时间服务器的端口
+
 	/*inet_pton in <arpa/inet.h>*/
-	if( inet_pton( AF_INET,argv[1], &servaddr.sin_addr ) <=0 )
+	if( inet_pton( AF_INET,ip_address, &servaddr.sin_addr ) <=0 )
 		err_sys("connect error");
+
 	Connect(sockfd,(SA *) &servaddr, sizeof(servaddr));
+
 	//只对第一个socket进行操作
 	str_cli(stdin,sockfd);
+
 	exit( 0 );/*exit() in <stdlib.h>*/
+
 	return 0;
 }
