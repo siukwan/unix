1c0bc65116edebccabfac15468def772a2a2a5b7
siukwanCentOS <pypy39353@qq.com>
2016-03-10 13:32:05 +0800

    修改IP

--- webServer/v1/server.cpp
+++ webServer/v1/server.cpp
@@ -4,22 +4,27 @@
 #include<sys/socket.h>
 #include<string.h>
 #include<unistd.h>
+#include<errno.h>
 //简单的服务器实现
 int main()
 {
 	printf("server is running...\n");
 	int listenFd,connectFd;
-	
+	int res;
 	listenFd = socket(AF_INET,SOCK_STREAM,0);//创建一个套接字
 
 	//设置地址和监听端口
 	sockaddr_in servAddr;//netinet/in.hi
 	bzero(&servAddr,sizeof(servAddr));//string.h
 	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");//把字符串的ip转换成网络序
+	servAddr.sin_addr.s_addr = INADDR_ANY;// inet_addr("127.0.0.1");//把字符串的ip转换成网络序
 	servAddr.sin_port = htons(8800);//arpa/inet.h 把short型从主机序转换成网络序
 	
-	bind(listenFd,(sockaddr*)&servAddr,sizeof(servAddr));//需要把sockaddr_in强制转换成sockaddr
+	res = bind(listenFd,(sockaddr*)&servAddr,sizeof(servAddr));//需要把sockaddr_in强制转换成sockaddr
+	if(res<0)
+	{
+		printf("bind error\n");
+	}
 	int backlog;
 	listen(listenFd,backlog);
 
