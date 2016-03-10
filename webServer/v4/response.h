061f40fe758f4ff8284fab6d86bb92c922cac14e
siukwan Mac <pypy39353@qq.com>
2016-03-11 00:53:40 +0800

    test

---
+++ webServer/v4/response.h
@@ -0,0 +1,20 @@
+#ifndef __RESPONSE__
+#define __RESPONSE__
+
+char GLOBAL_HTTP_RESPONSE[]=
+"HTTP/1.1 302 Found\r\n"
+"Server: nginx\r\n"
+"Date: Thu, 10 Mar 2016 16:38:27 GMT\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n"
+"Transfer-Encoding: chunked\r\n"
+"Connection: keep-alive\r\n"
+"Location: http://24me.sinaapp.com/login.php\r\n"
+"Expires: Thu, 19 Nov 1981 08:52:00 GMT\r\n"
+"Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\n"
+"Pragma: no-cache\r\n"
+"Via: 10.67.15.22\r\n"
+"siukwan webserver"
+;
+	
+
+#endif
